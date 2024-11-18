28cbd43b9b9d3833f35d06a59f7855ffe3ad3173
daviddanielsebollena <david.daniel.sebollena@gmail.com>
2024-11-18 10:13:01 +0800

    feature: first lesson

--- main.go
+++ main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"math/rand"
 	"math"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello")
+	x := 5
+	y := 5
+	sum := x + y
+
+	a := []int{0,1,2,3,4}
+	a = append(a, 500)
+
+	vertices := make(map[string]int)
+
+	vertices["triangle"] = 2
+	vertices["square"] = 3
+	vertices["dodecagon"] = 12
+
+	fmt.Println(vertices["triangle"])
+
+	fmt.Println("Hello, the sum is ", sum)
+
+	fmt.Println("Here is an array of numbers", a)
 
 	fmt.Println("The time is", time.Now())
 
@@ -17,4 +34,8 @@ func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(25))
 
 	fmt.Printf("Pi is %g \n", math.Pi)
+
+	fmt.Println("Hello JL")
+
+	fmt.Scanln()
 }
